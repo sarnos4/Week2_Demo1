9513bdbb959c1950669754d9c709adfd93ce01fa
Sam <Sam@Sams-MBP.fios-router.home>
2017-01-23 15:57:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sam/week2_demo1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sam.week2_demo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View view) {
+    }
+}
